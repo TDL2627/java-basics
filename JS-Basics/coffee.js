41f877c9b39f457817b0196441be720d6ffa89b1
TDL2627 <the.dark.lord.2627@gmail.com>
2021-11-22 13:23:12 +0200

    added input calu

--- JS-Basics/coffee.js
+++ JS-Basics/coffee.js
@@ -1,4 +1,36 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello! My name is TDL2627";
   }
-alert("this is amazing")
+
+
+// function addition(){
+//     let  a1 = parseInt(prompt("enter your first number"));
+//     let a2 = parseInt(prompt("enter your second number"));
+//     let result= parseInt(a1)+parseInt(a2);
+//     document.write(result);
+//     document.getElementById("ans").innerHTML=result;
+// }
+function addie(){
+    let a1 = parseInt(document.getElementById("in1").value);
+    let a2 = parseInt(document.getElementById("in2").value);
+    let result= a1+a2;
+    document.getElementById("ans").value=result;
+}
+function minnie(){
+    let a1 = parseInt(document.getElementById("in1").value);
+    let a2 = parseInt(document.getElementById("in2").value);
+    let result= a1-a2;
+    document.getElementById("ans").value=result;
+}
+function timmie(){
+    let a1 = parseInt(document.getElementById("in1").value);
+    let a2 = parseInt(document.getElementById("in2").value);
+    let result= a1*a2;
+    document.getElementById("ans").value=result;
+}
+function divvie(){
+    let a1 = parseInt(document.getElementById("in1").value);
+    let a2 = parseInt(document.getElementById("in2").value);
+    let result= a1/a2;
+    document.getElementById("ans").value=result;
+}
